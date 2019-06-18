d84faafd78751a5223c2326fffebfb9f407bf6bf
briancollins081 <briancollins081@gmail.com>
2019-06-18 17:02:06 +0300

    added new files, end of 18th June

--- articles.php
+++ articles.php
@@ -1,27 +1,6 @@
 <?php require_once 'cn/head.php'; ?>
 
-<style>
-  .title-abc-blue{
-    border-left: 18px solid rgba(0, 153, 204,.8); 
-    background-color: rgba(128, 128, 128, 1);
-  }
-  .body-abc-green{
-    border-left: 18px solid rgba(77, 153, 0,.5); 
-    border-radius: 5px;
-  }
-  .footer-abc-blue{
-    border-left: 18px solid rgba(0, 153, 204,.8); 
-    border-radius: 5px;
-  }
-  .rem-top-mrg{
-    margin-top: -20px;
-  }
-  .rem-hr-mrg{
-    margin-top: 0;
-    margin-bottom: 0;
-  }
-</style>
-
+<?php require_once 'cn/m-abc-styles.php'; ?>
 <body class="">
     <div class="wrapper ">
         <?php require_once 'cn/sidebar.php'; ?>
---
+++ cn/m-abc-styles.php
@@ -0,0 +1,21 @@
+<style>
+  .title-abc-blue{
+    border-left: 18px solid rgba(0, 153, 204,.8); 
+    background-color: rgba(128, 128, 128, 1);
+  }
+  .body-abc-green{
+    border-left: 18px solid rgba(77, 153, 0,.5); 
+    border-radius: 5px;
+  }
+  .footer-abc-blue{
+    border-left: 18px solid rgba(0, 153, 204,.8); 
+    border-radius: 5px;
+  }
+  .rem-top-mrg{
+    margin-top: -20px;
+  }
+  .rem-hr-mrg{
+    margin-top: 0;
+    margin-bottom: 0;
+  }
+</style>
