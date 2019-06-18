88b045021d52464f65e8eac2e6a1dcb1b753862a
briancollins081 <briancollins081@gmail.com>
2019-06-18 16:40:13 +0300

    added articles landing page for future use

---
+++ articles_details.php
@@ -0,0 +1,45 @@
+<!-- CV & Cover Letter -->
+<?php require_once 'cn/head.php'; ?>
+
+<body class="">
+    <div class="wrapper ">
+        <?php require_once 'cn/sidebar.php'; ?>
+        <div class="main-panel">
+            <!-- Navbar -->
+            <?php require_once 'cn/navbar.php'; ?>
+            <!-- End Navbar -->
+            <div class="panel-header panel-header-sm">
+            </div>
+            <div class="content">
+                <div class="row">
+                  <div class="col-md-12">
+                    <div class="card card-tasks">
+                      <div class="card-header">
+                        <h5 class="card-category">IT in B/S Practices</h5>
+                        <h6 class="card-title">Article #1: “Big Data and Social Networks"</h6>
+                      </div>
+                      <div class="card-body">
+                        <div class="table-full-width table-responsive">
+                            <p>
+                                 The Role of Big Data and Cloud Computing in Market Intelligence” is interesting and fascinating. Its main goal involved evaluating the utilization of information technology in business practices. The paper talks about the role of big data in business practices. It is important to note that big data plays a critical role in internally working toward improving employee advocacy and company’s efficiency. Collected big data is also essential in enabling market intelligence professionals to carry out effective competitive analysis and position their investments and campaigns in a way that outshines the competitors. This paper is relevant since it aims at improving business practices to ensure effectiveness and efficiency through data analysis. Since I am studying a business course, the paper will help me to understand whatever is required for effective competition in market full of different firms. Besides, the paper outlines the role of technology in social network and business competition, therefore, enables revolution of businesses. 
+                            </p>
+                        </div>
+                      </div>
+                      <div class="card-footer">
+                        <hr>
+                        <div class="stats">
+                          <i class="now-ui-icons loader_refresh spin"></i> Updated 2 minutes ago
+                        </div>
+                      </div>
+                  </div>
+                  </div>
+                </div>
+            </div>
+           <?php require_once 'cn/footer.php'; ?>
+        </div>
+    </div>
+</body>
+<!--   Core JS Files   -->
+<?php require_once 'cn/tail.php'; ?>
+
+</html>
